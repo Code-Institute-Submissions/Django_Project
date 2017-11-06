297ea80e1661f6baf6131a10905f0faed2477b9c
Hure Ferdous <hure_fer@yahoo.com>
2017-11-06 13:17:01 +0000

    Update products app admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','category','price','description','image','stock','created','updated','available',]
+    list_filter = ['created','updated','available','category']
+    list_editable = ['price','stock','available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
