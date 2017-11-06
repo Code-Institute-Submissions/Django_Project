12b6bb502a8624593dd12c0d8505b7c1f40552e1
Hure Ferdous <hure_fer@yahoo.com>
2017-11-06 12:51:07 +0000

    Create Product and Category Model in products app models.py

--- Gift/urls.py
+++ Gift/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url, include
-from paypal.standard.ipn import urls as paypal_urls
-from paypal_store import views as paypal_views
 from django.contrib import admin
 from customer import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.get_index, name='index'),
-    url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
-    url(r'^paypal-return', paypal_views.paypal_return),
-    url(r'^paypal-cancel', paypal_views.paypal_cancel),
+
+
 ]
