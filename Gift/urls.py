f09fee2565d424236282f8964c8dd9cb30598718
Hure Ferdous <hure_fer@yahoo.com>
2017-11-08 18:51:54 +0000

    Create Teplates for Products

--- Gift/urls.py
+++ Gift/urls.py
@@ -4,10 +4,13 @@ from customer import views
 from django.views.static import serve
 from .settings import MEDIA_ROOT
 
+
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.get_index, name='index'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^', include('products.urls', namespace='products')),
 
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
-def all_products(request):
-    products = Product.objects.all()
-    return render(request, "products/products.html", {"products": products})
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, "products/products_view/product_list.html",
+                  {"products": products,"category":category,"categories":categories})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, "products/products_view/product_detail.html",
+                  {"product": product, "cart_product_form":cart_product_form})
+
